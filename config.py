5294f8abd863632b126f00835936a169a088088a
ankush-1223 <rdxankush23@gmail.com>
2025-05-02 12:54:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv
 
-API_ID = int(os.getenv("API_ID", 123456))  # Replace with your default or leave for Keyob env
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_ID = int(os.getenv("OWNER_ID"))
