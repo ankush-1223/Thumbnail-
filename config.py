0b5c8d2036a3c777c30a5f849ffe67540189a2fb
ankush-1223 <rdxankush23@gmail.com>
2025-05-02 22:15:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-API_ID = int(os.getenv("API_ID"))
+API_ID = os.getenv("API_ID")  # Set these as environment variables
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID"))
